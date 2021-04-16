3c6876c9a8b700e10e2c5835313e79afafd659de
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-16 10:13:52 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+//1
+let nombre = 8.56;
+//2
+let nombreRound = Math.round(nombre);
+console.log(nombreRound);
+//3
+let nombreCeil = Math.ceil(nombre);
+console.log(nombreCeil);
+//4
+let nombreFloor = Math.floor(nombre);
+console.log(nombreFloor);
+//5
+let nombreTrunc = Math.trunc(nombre);
+console.log(nombreTrunc);
+//6
+console.log(Math.random());
+//7
+console.log(Math.random()*100);
+//8
+console.log(Math.trunc(Math.random()*100));
+//9
+console.log(Math.pow(8,2));
+//10
+console.log(Math.sqrt(9));
+//11
+console.log(Math.abs(-1));
+//12
+console.log(Math.max(-2, 1000, 8, 57));
+//13
+console.log(Math.min(-2, 1000, 8, 57));
+
